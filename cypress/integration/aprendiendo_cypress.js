382c91265fda3f2dd4f7e4a0f8e44045c30171b6
Agostina <agos.bertot@gmail.com>
2021-05-11 23:16:20 -0300

    Primeros casos de prueba en http://automationpractice.com/index.php

---
+++ cypress/integration/aprendiendo_cypress.js
@@ -0,0 +1,10 @@
+/// <reference types="Cypress"/> 
+
+describe('Primeros casos de prueba', () => {
+
+    it('Contabilizar cantidad de elementos en la pagina principal', () => {
+        cy.visit('http://automationpractice.com/index.php')
+        
+    })
+
+})
